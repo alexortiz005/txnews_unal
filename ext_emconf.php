caddccdac0f3fee1af307b9a92f03b0359ad0b8e
Alex Ortiz <eaortiz@unal.edu.co>
2018-12-04 16:50:58 -0500

    entrega final

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,8 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '8.6.0-8.7.99',
-            'plantilla_unal' => '1.0-1.9',
-            'bootstrap_package' => '10.0.0-10.0.99,',
+            'bootstrap_package' => '10.0.0-10.0.99',
             'unal_skin_default' => '0.2.0-0.2.99',
             'news' => '6.3.0-6.3.99',
         ],
